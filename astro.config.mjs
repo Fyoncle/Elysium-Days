da806cb5b04934b251113540f340497b2efcd77d
Fyoncle <63975256+Fyoncle@users.noreply.github.com>
2026-09-26 18:53:46 +0300

    Fix Banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig({
             tag: "meta",
             attrs: {
               property: "og:image",
-              content: "https://elysium-days-wiki.netlify.app/banner.png",
+              content: "https://fyoncle.github.io/Elysium-Days/banner.png"",
             },
           },
           {
